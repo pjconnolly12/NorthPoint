48782417376ea10c178c5686c26510b951fb564a
unknown <pjconnolly12@gmail.com>
2019-02-07 21:47:14 -0500

    updates per feedback

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ function navBar() {
   }
 }
 
-var prevScrollPos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if ( prevScrollPos > currentScrollPos){
-        document.getElementById('myNav').style.top = '0';  
-    } else {
-        document.getElementById('myNav').style.top = '-50px';
-    }
-    prevScrollPos = currentScrollPos;
-}
+// var prevScrollPos = window.pageYOffset;
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//     if ( prevScrollPos > currentScrollPos){
+//         document.getElementById('myNav').style.top = '0';  
+//     } else {
+//         document.getElementById('myNav').style.top = '-50px';
+//     }
+//     prevScrollPos = currentScrollPos;
+// }
 
 function formSubmit() {
   var form = document.getElementById("form");
